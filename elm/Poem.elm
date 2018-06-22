cc6ee121dcf414b5feaeeaf2c6142198ba1d4c4d
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2018-06-22 15:02:48 -0400

    produced query string on front end and qrote route to fetch word dictionary from phrasefinder

--- elm/Poem.elm
+++ elm/Poem.elm
@@ -1,7 +1,8 @@
-module Poem exposing (Poem, poemFromText)
+module Poem exposing (Poem, chunkEvery, poemFromText, toQueryStrings, wordsToQueryString)
 
 import Json.Decode as JD
 import Regex
+import Set exposing (Set)
 
 
 type alias Poem =
@@ -14,6 +15,54 @@ type alias Line =
 
 type alias Word =
     String
+
+
+toQueryStrings : Poem -> List String
+toQueryStrings =
+    uniqueWords >> chunkEvery 3 >> List.map wordsToQueryString
+
+
+
+--     -- get unique words
+--     -- chunk
+--     -- convert chunk to query string
+
+
+wordsToQueryString : List Word -> String
+wordsToQueryString words =
+    let
+        base =
+            "http://phrasefinder.io/search?corpus=eng-gb&query="
+    in
+    base ++ String.join "/" words
+
+
+chunkEvery : Int -> List a -> List (List a)
+chunkEvery n =
+    List.foldl
+        (\val acc ->
+            case acc of
+                [] ->
+                    [ val ] :: acc
+
+                x :: xs ->
+                    if List.length x == n then
+                        [ val ] :: acc
+                    else
+                        (x ++ [ val ]) :: xs
+        )
+        []
+        >> List.reverse
+
+
+uniqueWords : Poem -> List Word
+uniqueWords =
+    List.concat >> unique
+
+
+unique : List comparable -> List comparable
+unique =
+    Set.fromList >> Set.toList
 
 
 dePunctuate : String -> String
--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const app = express();
 const publicPath = path.join(__dirname, './public');
 const indexHtmlPath = path.join(__dirname, './public/index.html');
 
 app.use(express.static(publicPath));
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(indexHtmlPath);
 });
 
-app.get('/test', (req, res) => {
-  request('http://phrasefinder.io/search?query=shall&corpus=eng-gb', (err, response, body) => {
-    res.send(JSON.parse(body));
+app.put('/word-years', (req, res) => {
+  console.log(req.body)
+
+  request(req.body.queryString, (err, response, body) => {
+      console.log(body)
+
+      body = JSON.parse(body)
+
+
+    // const rawJSON = JSON.parse(body);
+    const wordDict = {};
+    body.phrases.forEach((phrase)=>{
+        wordDict[phrase.tks[0].tt] = phrase.fy;
+    })
+    res.send(wordDict);
   })
 });
 
